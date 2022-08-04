76759f8f603d5e0f4881faf898291d4a2fd7feff
nazarkohut <85364294+nazarkohut@users.noreply.github.com>
2022-08-04 19:24:06 +0300

    Changed logo. Home page is responsive now and has new describe text.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+// import {MatIconRegistry} from "@angular/material/icon";
+import {DomSanitizer} from "@angular/platform-browser";
+import {MatIconRegistry} from "@angular/material/icon";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AniGame';
+
+  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
+    let customIcons: Array<{name: string, url: string}> = [{name: 'ani-game-logo', url: '../assets/ani-game-logo.svg'}]
+    console.log(customIcons.length);
+    for (let i = 0; i < customIcons.length; i++){
+      let currentIcon = customIcons[i];
+      matIconRegistry.addSvgIcon(currentIcon.name, this.domSanitizer.bypassSecurityTrustResourceUrl(currentIcon.url))
+    }
+  }
 }
